e332b05a2e2dcab715276d8eac172e4d930ca537
zahed01x <zahed01x@gmail.com>
2024-05-10 01:55:23 +0600

    updated

--- appoint.js
+++ appoint.js
@@ -1,29 +1,24 @@
 
-  // Wait for the DOM to be fully loaded
-  document.addEventListener('DOMContentLoaded', function () {
-    // Get the form element
-    var form = document.querySelector('.formbold-form-wrapper form');
 
-    // Add event listener for form submission
+  document.addEventListener('DOMContentLoaded', function () {
+
+    var form = document.querySelector('.formbold-form-wrapper form');
     form.addEventListener('submit', function (event) {
-      // Prevent the default form submission behavior
       event.preventDefault();
 
-      // Validate form fields
+
       if (validateForm()) {
-        // If the form is valid, submit it
         form.submit();
       }
     });
-
-    // Function to validate form fields
+ds
     function validateForm() {
       var name = document.getElementById('name').value;
       var phone = document.getElementById('phone').value;
       var age = document.getElementById('age').value;
       var email = document.getElementById('email').value;
 
-      // Perform basic validation (you can add more complex validation as needed)
+      
       if (name.trim() === '') {
         alert('Please enter your full name');
         return false;
@@ -41,9 +36,7 @@
         return false;
       }
       
-      // Optionally, you can perform additional validation for other fields
-      
-      // If all validations pass, return true
+
       return true;
     }
   });
